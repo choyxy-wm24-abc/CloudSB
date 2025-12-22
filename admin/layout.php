82ed8e870941fd8448475256bec6454410979fc4
choyxy-wm24-abc <choyxy-wm24@student.tarc.edu.my>
2025-12-22 17:51:21 +0800

    haleluya

--- admin/layout.php
+++ admin/layout.php
@@ -21,15 +21,6 @@ auth('Admin');
             </a>
         </div>
         
-        <nav class="admin-menu">
-            <ul class="admin-nav-list">
-                <li><a href="../admin/admin.php" class="admin-nav-link">Dashboard</a></li>
-                <li><a href="../admin/product.php" class="admin-nav-link">Products</a></li>
-                <li><a href="../admin/member.php" class="admin-nav-link">Members</a></li>
-                <li><a href="../admin/order.php" class="admin-nav-link">Orders</a></li>
-                <li><a href="../admin/setting.php" class="admin-nav-link">Settings</a></li>
-            </ul>
-        </nav>
         
         <div class="admin-actions">
             <a href="../index.php" class="admin-nav-link">
